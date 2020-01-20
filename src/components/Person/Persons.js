92f7ad91a85389ac7224b11fb561c9246d2a0fd3
Marius <mario.luk4@gmail.com>
2020-01-20 20:45:07 +0100

    lifecycle uptade s Hookmi

--- src/components/Person/Persons.js
+++ src/components/Person/Persons.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { Component} from 'react';
 import Person from "./Person";
 
-const persons = (props) => props.persons.map((person, index) => {
-    console.log('person rendering');
-        return (
-            <Person
-                click={() => props.clicked(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={event => props.changed(event, person.id)}
-            />
-        );
-    });
+class Persons extends Component{
+    // static getDerivedStateFromProps(props,state){
+    //     console.log('getDerivedStateFromProps')
+    //     return state;
+    // }
 
-export default persons;
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('shoulComponentUptade');
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('getSnapshotBeforeUpdate');
+        return {message: 'snapshot!'};
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+      console.log('consoleDidUptade');
+    }
+
+    render(){
+        console.log('person rendering');
+        return this.props.persons.map((person, index) => {
+            return (
+                <Person
+                    click={() => this.props.clicked(index)}
+                    name={person.name}
+                    age={person.age}
+                    key={person.id}
+                    changed={event => this.props.changed(event, person.id)}
+                />
+            );
+        });
+    }
+}
+
+
+export default Persons;
